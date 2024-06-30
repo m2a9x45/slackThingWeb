1e9482590016e530a7962a1519675b8310f02e27
Lewis <dukelowlewis@gmail.com>
2024-06-30 22:08:19 +0100

    Init 🚀

---
+++ src/routes/+page.svelte
@@ -0,0 +1,58 @@
+<script>
+    async function onSubmit(e) {
+        const formData = new FormData(e.target);
+
+        const data = {};
+        for (let field of formData) {
+            const [key, value] = field;
+            data[key] = value;
+        }
+
+        const response = await fetch("http://localhost:5000/slash", {
+            method: "POST",
+            headers: {
+                "content-type": "application/json",
+            },
+            body: JSON.stringify(data),
+        });
+        const resData = await response.json();
+        console.log(resData);
+    }
+</script>
+
+<main>
+    <form on:submit|preventDefault={onSubmit}>
+        <label for="">Escalation ID</label>
+        <input name="path_id" type="text" placeholder="path_id" />
+        <label for="">Slash command</label>
+        <input name="command" type="text" placeholder="slash command" />
+        <label for="">Workflow ID</label>
+        <input name="workflow_id" type="text" placeholder="workflow_id" />
+        <input type="submit" value="submit" />
+    </form>
+</main>
+
+<style>
+    main {
+        width: 45%;
+        margin: 5rem auto;
+    }
+
+    input[type="text"] {
+        width: 100%;
+        padding: 1em;
+        margin: 0.5em 0;
+        box-sizing: border-box;
+    }
+
+    input[type="submit"] {
+        background-color: #243f90;
+        border: none;
+        color: white;
+        padding: 15px 32px;
+        text-align: center;
+        text-decoration: none;
+        display: inline-block;
+        font-size: 16px;
+    }
+</style>
