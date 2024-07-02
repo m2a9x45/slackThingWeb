ad8b88746b440dd198a3b9b529e1575575bb90ea
Lewis <dukelowlewis@gmail.com>
2024-07-03 00:14:04 +0100

    Adds signin with slack button / redirect handling

---
+++ src/routes/oauth/slack/+page.svelte
@@ -0,0 +1,21 @@
+<script>
+    import { onMount } from "svelte";
+
+    onMount(async () => {
+        const urlParams = new URLSearchParams(window.location.search);
+        const code = urlParams.get("code");
+        console.log(code);
+
+        const response = await fetch("http://localhost:5000/oauth/slack", {
+            method: "POST",
+            headers: {
+                "content-type": "application/json",
+            },
+            body: JSON.stringify({ code: code }),
+        });
+        const resData = await response.json();
+        console.log(resData);
+    });
+</script>
+
+<h1>Hello, Slack redirect</h1>
