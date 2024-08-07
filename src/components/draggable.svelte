def99d0d562c9578ff279cf7a0f4e08e7e9f0b5a
Lewis <dukelowlewis@gmail.com>
2024-08-07 18:33:21 +0100

    WIP dragable

---
+++ src/components/draggable.svelte
@@ -0,0 +1,69 @@
+<script>
+    export let left = 100;
+    export let top = 100;
+
+    $: innerWidth = 0;
+    $: innerHeight = 0;
+
+    export let lines;
+
+    let moving = false;
+
+    function onMouseDown() {
+        moving = true;
+    }
+
+    function onMouseMove(e) {
+        if (moving) {
+            left += e.movementX;
+            top += e.movementY;
+
+            if (top < 0) {
+                top = 0;
+            }
+
+            if (left < 0) {
+                left = 0;
+            }
+
+            if (left > innerWidth - 100) {
+                left = innerWidth - 100;
+            }
+
+            if (top > innerHeight - 75) {
+                top = innerHeight - 75;
+            }
+        }
+    }
+
+    function onMouseUp(e) {
+        moving = false;
+        lines = { x: left, y: top };
+    }
+
+    // 	$: console.log(moving);
+</script>
+
+<div
+    on:mousedown={onMouseDown}
+    style="left: {left}px; top: {top}px;"
+    class="draggable"
+>
+    <slot></slot>
+</div>
+
+<svelte:window
+    on:mouseup={onMouseUp}
+    on:mousemove={onMouseMove}
+    bind:innerWidth
+    bind:innerHeight
+/>
+
+<style>
+    .draggable {
+        user-select: none;
+        cursor: move;
+        border: solid 1px gray;
+        position: absolute;
+    }
+</style>
---
+++ src/routes/wf/[id]/+page.svelte
@@ -0,0 +1,65 @@
+<script>
+    import Draggable from "../../../components/draggable.svelte";
+    // import { onMount } from "svelte";
+
+    // onMount(() => {
+    //     readWorkflow();
+    // });
+
+    // export let steps = [];
+
+    // async function readWorkflow() {
+    //     const response = await fetch(`http://localhost:5000/wf`, {
+    //         credentials: "include",
+    //     });
+    //     const data = await response.json();
+    //     console.log(data);
+    //     steps = data;
+    // }
+
+    let lines = {};
+
+    function onMouseUp(e) {
+        console.log(lines);
+    }
+</script>
+
+<!-- {#each steps as step}
+    <div>
+        <h4>StepID: {step.step_id}</h4>
+        <p>Type: {step.action}</p>
+        <p>Message: {step.message}</p>
+        <div>
+            <ul>
+                {#each step.branches as branch}
+                    <li>{branch.text} -> {branch.next_step_id}</li>
+                {/each}
+            </ul>
+        </div>
+    </div>
+{/each} -->
+
+<!-- https://stackoverflow.com/questions/64604624/programatically-get-svelte-component-instance -->
+
+<main>
+    <Draggable bind:lines>
+        <h1>Drag Me</h1>
+    </Draggable>
+    <Draggable>
+        <h1>Drag Me 2</h1>
+    </Draggable>
+
+    <svg width="500" height="500"
+        ><line x1="50" y1="50" x2="350" y2="50" stroke="red" /></svg
+    >
+</main>
+
+<svelte:window on:mouseup={onMouseUp} />
+
+<style>
+    main {
+        width: 100%;
+        height: 100%;
+        margin: 1rem auto;
+    }
+</style>
