5edc96167a21d356075ef629d47fd25313456023
Lewis Dukelow <35412216+m2a9x45@users.noreply.github.com>
2024-08-10 15:54:06 +0100

    Got reactivity working for the inspector component

---
+++ src/components/inspector.svelte
@@ -0,0 +1,41 @@
+<script>
+  export let step;
+
+  let selectedStep;
+
+  $: {
+    console.log(step);
+    selectedStep = step;
+  }
+
+  function updateWorkflow() {
+    console.log("hi: " + selectedStep.step_id);
+  }
+</script>
+
+<div>
+  <h4>{selectedStep.step_id}</h4>
+  <div>
+    <!-- <select bind:value={step.action} name="cars" id="cars">
+      {#each ["button_selection", "send_message", "open_model"] as option}
+        <option value={option}>{option}</option>
+      {/each}
+    </select> -->
+
+    <textarea bind:value={selectedStep.message} type="text" />
+
+    <!-- <ul>
+      {#each step.branches as branch}
+        <li>{branch.text} -> {branch.next_step_id}</li>
+      {/each}
+    </ul> -->
+
+    <button on:click={updateWorkflow}>Save</button>
+  </div>
+</div>
+
+<style>
+  textarea {
+    width: 90%;
+  }
+</style>
--- src/routes/wf/[id]/+page.svelte
+++ src/routes/wf/[id]/+page.svelte
@@ -1,33 +1,38 @@
 <script>
   import Draggable from "../../../components/draggable.svelte";
+  import Inspector from "../../../components/inspector.svelte";
+
+  import { onMount } from "svelte";
+
   import { get } from "svelte/store";
   import { lines } from "./linesStore";
-  // import { onMount } from "svelte";
 
-  // onMount(() => {
-  //     readWorkflow();
-  // });
+  onMount(() => {
+    readWorkflow();
+  });
 
-  // export let steps = [];
+  export let steps = [];
 
-  // async function readWorkflow() {
-  //     const response = await fetch(`http://localhost:5000/wf`, {
-  //         credentials: "include",
-  //     });
-  //     const data = await response.json();
-  //     console.log(data);
-  //     steps = data;
-  // }
+  const spacing = [200, 600, 1000];
+
+  let showSidebar = false;
 
   let x1;
   let x2;
   let y1;
   let y2;
 
-  function onMouseUp(e) {
-    console.log(get(lines));
-    // console.log(get(lines).get("123").x);
+  async function readWorkflow() {
+    const response = await fetch(`http://localhost:5000/wf`, {
+      credentials: "include",
+    });
+    const data = await response.json();
+    console.log(data);
+    steps = data;
+  }
 
+  function onMouseUp(e) {
+    // console.log(get(lines));
     const things = get(lines);
 
     x1 = things.get("123").x;
@@ -36,34 +41,50 @@
     x2 = things.get("456").x;
     y2 = things.get("456").y;
   }
+
+  let selectedStep;
+  function editStep(step) {
+    showSidebar = true;
+    selectedStep = step;
+  }
 </script>
 
-<!-- {#each steps as step}
-    <div>
-        <h4>StepID: {step.step_id}</h4>
-        <p>Type: {step.action}</p>
-        <p>Message: {step.message}</p>
-        <div>
-            <ul>
-                {#each step.branches as branch}
-                    <li>{branch.text} -> {branch.next_step_id}</li>
-                {/each}
-            </ul>
-        </div>
-    </div>
-{/each} -->
-
-<!-- https://stackoverflow.com/questions/64604624/programatically-get-svelte-component-instance -->
-
 <main>
-  <Draggable id={"123"}>
-    <h1>Drag Me</h1>
-  </Draggable>
-  <Draggable id={"456"}>
-    <h1>Drag Me 2</h1>
-  </Draggable>
+  <div class="content">
+    {#each steps as step, i}
+      <Draggable id={step.step_id} left={spacing[i]}>
+        <div>
+          <h4>StepID: {step.step_id}</h4>
+          <p>Type: {step.action}</p>
+          <p>Message: {step.message}</p>
+          <div>
+            <ul>
+              {#each step.branches as branch}
+                <li>{branch.text} -> {branch.next_step_id}</li>
+              {/each}
+            </ul>
+          </div>
+          <button on:click={editStep(step)}>Edit</button>
+        </div>
+      </Draggable>
+    {/each}
 
-  <svg><line {x1} {y1} {x2} {y2} stroke="red" /></svg>
+    <Draggable id={"123"} top={400}>
+      <h1>Drag Me</h1>
+    </Draggable>
+    <Draggable id={"456"} left={400} top={400}>
+      <h1>Drag Me 2</h1>
+    </Draggable>
+
+    <svg><line {x1} {y1} {x2} {y2} stroke="red" /></svg>
+  </div>
+
+  {#if showSidebar}
+    <div class="sidebar">
+      <h1>Side Bar</h1>
+      <Inspector step={selectedStep}></Inspector>
+    </div>
+  {/if}
 </main>
 
 <svelte:window on:mouseup={onMouseUp} />
@@ -72,12 +93,23 @@
   :global(body) {
     width: 100vw;
     height: 100vh;
+    margin: 0;
   }
 
   main {
+    display: flex;
     width: 100%;
     height: 100%;
-    margin: 1rem auto;
+  }
+
+  .content {
+    width: 90vw;
+  }
+
+  .sidebar {
+    width: 20%;
+    height: 100vh;
+    background-color: rgb(206, 231, 246);
   }
 
   svg {
